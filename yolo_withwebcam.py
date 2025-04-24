f424e5401f06cfb45bbb17d6856fba6a914f28d2
Mudassirkhan17 <mudassir.nedian@gmail.com>
2025-04-25 03:31:27 +0500

    yolo with webcam done

--- yolo_withwebcam.py
+++ yolo_withwebcam.py
@@ -1,8 +1,12 @@
 import cv2
 from ultralytics import YOLO
+import math
+import cvzone
+
 
 model = YOLO("yolov8n.pt")
 
+classname = ["Person","bicycle","car","motorcycle","airplane","bus","train","truck","boat","traffic light","fire hydrant","stop sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot","hot dog","pizza","donut","cake","chair","couch","potted plant","bed","dining table","toilet","tv","laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink","refrigerator","book","clock","vase","scissors","teddy bear","hair drier","toothbrush"]
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -11,8 +15,22 @@ cap.set(4, 640)
 while True:
     ret, frame = cap.read()
     results = model(frame,stream=True)
-    annotated_frame = results[0].plot()
-    cv2.imshow("YOLO", annotated_frame)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            x1,y1,x2,y2 = box.xyxy[0]
+            x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+            w,h = x2-x1,y2-y1
+
+            cvzone.cornerRect(frame,(x1,y1,w,h),l=9,rt=5)
+            conf = math.ceil((box.conf[0]*100))/100
+            cls = int(box.cls[0])
+            cvzone.putTextRect(frame,f"{classname[cls]} {conf}",(max(0,x1),max(35,y1)),scale=1,thickness=2,offset=5)
+
+
+            # cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
+            
+    cv2.imshow("YOLO", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
